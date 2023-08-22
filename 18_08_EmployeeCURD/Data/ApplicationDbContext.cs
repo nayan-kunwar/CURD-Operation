2a378ddac0698417a1762a74eff7314e2c1b4de0
Nayan Kunwar <54805418+nayan-kunwar@users.noreply.github.com>
2023-08-22 11:48:56 +0530

    Initial commit

---
+++ 18_08_EmployeeCURD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using _18_08_EmployeeCURD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _18_08_EmployeeCURD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ 18_08_EmployeeCURD/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _18_08_EmployeeCURD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        public DateTime? RecordCreatedOn { get; set; }
+    }
+}
